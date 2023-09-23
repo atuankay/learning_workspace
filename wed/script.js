9dd706ba7c3bd395ff3f9b3e158a2a38296830c7
A Tuan <atuan7535@gmail.com>
2023-09-23 10:35:46 +0700

    Upload

---
+++ wed/script.js
@@ -0,0 +1,14 @@
+let usernameRef = document.getElementById("username");
+let passwordRef = document.getElementById("password");
+let eyeL = document.querySelector(".eyeball-l");
+let eyeR = document.querySelector(".eyeball-r");
+let handL = document.querySelector(".hand-l");
+let handR = document.querySelector(".hand-r");
+let normalEyeStyle = () => {
+    eyeL.style.cssText = "left: 0.6em; top: 0.6em;";
+    eyeR.style.cssText = "right: 0.6em; top: 0.6em;";
+};
+let normalHandStyle = () => {
+    handL.style.cssText = "height: 2.81em; top: 8.4em; left: 7.5em; transform: rotate(0deg);";
+    handR.style.cssText = "height: 2.81em; top: 8.4em; right: 7.5em; transform: rotate(0deg);";
+};
